98b65ebdd70382a7309656ad84865c19dca4dae9
Muhammad Raheel <113178849+raheelimran-10P@users.noreply.github.com>
2023-10-19 14:23:30 +0500

    create test case

--- Web/Pages/demo_qa_page.py
+++ Web/Pages/demo_qa_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import expect, Page, Browser
 from Web.TestData.test_data import TestData
 import time
+import logging
 
 class DemoQaPage:
 
@@ -16,6 +17,14 @@ class DemoQaPage:
         self._upload_and_download = page.get_by_text("Upload and Download")
         self._button_download = page.locator('#downloadButton')
         self._select_file = page.locator('#uploadFile')
+        self._form = page.get_by_text("Forms")
+        self._practice_form = page.get_by_text("Practice Form")
+        self._first_name = page.locator('#firstName')
+        self._last_name = page.locator('#lastName')
+        self._user_email = page.locator('#userEmail')
+        self._male = page.get_by_text("Male", exact=True)
+        self._user_number = page.get_by_placeholder("Mobile Number")
+        self._hobbie = page.get_by_text("Sports")
 
     def nagivate_to(self, url):
         self.page.goto(url)
@@ -53,7 +62,39 @@ class DemoQaPage:
         download = download_info.value
         download.save_as(path+"\\"+ download.suggested_filename)
         expect(self._select_file).to_be_visible()  
-        self._select_file.set_input_files(path+"\\"+file_name, timeout=TestData.TIMEOUT_IN_MILLISECONDS)        
+        self._select_file.set_input_files(path+"\\"+file_name, timeout=TestData.TIMEOUT_IN_MILLISECONDS)  
+
+    def practice_form(self, first_name="first name", 
+                            last_name="last name", 
+                            user_email="abc@gmail.com", 
+                            user_number="03546265668",
+                            subject="subject"
+                        ):
+        self._form.click()
+        expect(self._practice_form).to_be_visible()  
+        self._practice_form.click()
+        expect(self._first_name).to_be_visible()  
+        expect(self._first_name).to_be_editable()
+        self._first_name.clear()
+        self._first_name.fill(first_name)
+        expect(self._last_name).to_be_visible()  
+        expect(self._last_name).to_be_editable()
+        self._last_name.clear()
+        self._last_name.fill(last_name)
+        expect(self._user_email).to_be_visible()  
+        expect(self._user_email).to_be_editable()
+        self._user_email.clear()
+        self._user_email.fill(user_email)
+        expect(self._male).to_be_visible()        
+        if not self._male.is_checked():
+            self._male.check()
+        expect(self._user_number).to_be_visible()  
+        expect(self._user_number).to_be_editable()
+        self._user_number.clear()
+        self._user_number.fill(user_number)
+        if not self._hobbie.is_checked():
+            self._hobbie.check()
+
 
 
 
